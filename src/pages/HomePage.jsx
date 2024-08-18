d5f90b6c41187ee5eeb28907ba5f49b59a801941
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-08-18 13:25:46 +0200

    up

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getTrendingMovies } from '../../services/api';
 import MovieList from '../../components/MovieList/MovieList';
+import styles from './HomePage.module.css';
 
 const HomePage = () => {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        getTrendingMovies().then(setMovies);
-    }, []);
+  useEffect(() => {
+    getTrendingMovies().then(setMovies);
+  }, []);
 
-    return (
-        <div>
-            <h1>Trending Movies</h1>
-            <MovieList movies={movies} />
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1 className={styles.title}>Trending Movies</h1>
+      <MovieList movies={movies} />
+    </div>
+  );
 };
 
 export default HomePage;
