5e6d157487b9692ba5520c4e3f86bbe27212e834
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-08-18 14:05:11 +0200

    up

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import Navigation from './Navigation/Navigation';
+import styles from './HomePage.module.css';
 
-import React, { useEffect, useState } from 'react';
-import styles from './HomePage.module.css'; 
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 
-const HomePage = () => {
-    
+function App() {
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Home Page</h1>
-            {}
-        </div>
+        <Router>
+            <Navigation />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/movies" element={<MoviesPage />} />
+                    <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </Suspense>
+        </Router>
     );
-};
+}
 
-export default HomePage;
+export default App;
