b1ba52fd5aa254a625c3fd3782df4f66d0d95878
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-09-04 16:46:11 +0200

    up

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,30 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, Link, useHistory } from 'react-router-dom';
-import { getMovieDetails } from '../../services/api';
+import { Link, useParams } from 'react-router-dom';
+import MovieCast from '../../components/MovieCast/MovieCast';
+import MovieReviews from '../../components/MovieReviews/MovieReviews';
 import styles from './MovieDetailsPage.module.css';
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
-  const history = useHistory();
-  const [movie, setMovie] = useState(null);
-
-  useEffect(() => {
-    getMovieDetails(movieId).then(setMovie);
-  }, [movieId]);
-
-  if (!movie) return null;
+  
 
   return (
-    <div className="container">
-      <button className={styles.backButton} onClick={() => history.goBack()}>Go Back</button>
-      <div className={styles.container}>
-        <img className={styles.poster} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-        <div className={styles.details}>
-          <h1>{movie.title}</h1>
-          <p>{movie.overview}</p>
-          {}
-        </div>
-      </div>
+    <div className={styles.container}>
+      <Link to="/" className={styles.goBack}>Go back</Link>
+      <h1 className={styles.title}>Movie Title</h1>
+      {}
+      <MovieCast />
+      <MovieReviews />
     </div>
   );
 };
