27b201eb248b2ceab34be52499bcab2c75aa43ac
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-09-04 16:47:34 +0200

    up

---
+++ src/components/MovieCast/MovieCast.jsx
@@ -0,0 +1,13 @@
+import styles from './MovieCast.module.css';
+
+const MovieCast = () => {
+
+  return (
+    <div className={styles.cast}>
+      <h2 className={styles.title}>Cast</h2>
+      {}
+    </div>
+  );
+};
+
+export default MovieCast;
---
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -0,0 +1,13 @@
+import styles from './MovieReviews.module.css';
+
+const MovieReviews = () => {
+  
+  return (
+    <div className={styles.reviews}>
+      <h2 className={styles.title}>Reviews</h2>
+      {}
+    </div>
+  );
+};
+
+export default MovieReviews;
